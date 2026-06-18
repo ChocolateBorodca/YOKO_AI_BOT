a3347946a38f03cc3fc7f44c3cb1ac7395df8fc4
ChocolateBorodca <ashatkhabibullin118@gmail.com>
2026-06-19 01:15:49 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+import re
+import requests
+
+def translate_to_burmalda(text):
+    text = re.sub(r'\bя\b', 'ч', text, flags=re.IGNORECASE)
+    text = re.sub(r'\bдед\b', 'дод', text, flags=re.IGNORECASE)
+    text = re.sub(r'\bдеда\b', 'дода', text, flags=re.IGNORECASE)
+    text = re.sub(r'\bжена\b', 'жинка', text, flags=re.IGNORECASE)
+    text = re.sub(r'\bжены\b', 'жинки', text, flags=re.IGNORECASE)
+    
+    words = text.split()
+    burmalda_words = []
+    for word in words:
+        clean_word = re.sub(r'[^\w\s]', '', word)
+        if len(clean_word) > 2 and not clean_word.lower() in ['как', 'что', 'или', 'под', 'для', 'без', 'все']:
+            if word.endswith(('.', ',', '!', '?')):
+                mark = word[-1]
+                w = word[:-1]
+                w = w + 'сть' if w.endswith(('а', 'я', 'ь', 'о', 'е')) else w + 'ость'
+                burmalda_words.append(w + mark)
+            else:
+                w = word + 'сть' if word.endswith(('а', 'я', 'ь', 'о', 'е')) else word + 'ость'
+                burmalda_words.append(w)
+        else:
+            burmalda_words.append(word)
+    return " ".join(burmalda_words)
+
+def transcribe_audio(audio_bytes, hf_token):
+    try:
+        API_URL = "https://huggingface.co"
+        headers = {"Authorization": f"Bearer {hf_token}"}
+        response = requests.post(API_URL, headers=headers, data=audio_bytes)
+        return response.json().get("text", "")
+    except:
+        return ""
